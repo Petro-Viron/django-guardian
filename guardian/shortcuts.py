fe47af55df0128a371d90ddf06a8729cd9a1d5d7
Hasan Ammar <ammarh@gmail.com>
2015-08-02 19:27:47 -0400

    Add a function to look up perms for multiple groups
    Helps speed up cases where a user is a member of a large number of groups.

--- guardian/shortcuts.py
+++ guardian/shortcuts.py
@@ -624,3 +624,139 @@ def get_objects_for_group(group, perms, klass=None, any_perm=False, accept_globa
             pk_list.append(pk)
     objects = queryset.filter(pk__in=pk_list)
     return objects
+
+def get_objects_for_groups(groups, perms, klass=None, any_perm=False, accept_global_perms=True):
+    """
+    Returns queryset of objects for which a given ``group`` has *all*
+    permissions present at ``perms``.
+
+    :param group: ``Group`` instance for which objects would be returned.
+    :param perms: single permission string, or sequence of permission strings
+      which should be checked.
+      If ``klass`` parameter is not given, those should be full permission
+      names rather than only codenames (i.e. ``auth.change_user``). If more than
+      one permission is present within sequence, their content type **must** be
+      the same or ``MixedContentTypeError`` exception would be raised.
+    :param klass: may be a Model, Manager or QuerySet object. If not given
+      this parameter would be computed based on given ``params``.
+    :param any_perm: if True, any of permission in sequence is accepted
+    :param accept_global_perms: if ``True`` takes global permissions into account.
+      If any_perm is set to false then the intersection of matching objects based on global and object based permissions
+      is returned. Default is ``True``.
+
+    :raises MixedContentTypeError: when computed content type for ``perms``
+      and/or ``klass`` clashes.
+    :raises WrongAppError: if cannot compute app label for given ``perms``/
+      ``klass``.
+
+    Example:
+
+    Let's assume we have a ``Task`` model belonging to the ``tasker`` app with
+    the default add_task, change_task and delete_task permissions provided
+    by Django::
+
+        >>> from guardian.shortcuts import get_objects_for_group
+        >>> from tasker import Task
+        >>> group = Group.objects.create('some group')
+        >>> task = Task.objects.create('some task')
+        >>> get_objects_for_group(group, 'tasker.add_task')
+        []
+        >>> from guardian.shortcuts import assign_perm
+        >>> assign_perm('tasker.add_task', group, task)
+        >>> get_objects_for_group(group, 'tasker.add_task')
+        [<Task some task>]
+
+    The permission string can also be an iterable. Continuing with the previous example:
+        >>> get_objects_for_group(group, ['tasker.add_task', 'tasker.delete_task'])
+        []
+        >>> assign_perm('tasker.delete_task', group, task)
+        >>> get_objects_for_group(group, ['tasker.add_task', 'tasker.delete_task'])
+        [<Task some task>]
+
+    Global permissions assigned to the group are also taken into account. Continuing with previous example:
+        >>> task_other = Task.objects.create('other task')
+        >>> assign_perm('tasker.change_task', group)
+        >>> get_objects_for_group(group, ['tasker.change_task'])
+        [<Task some task>, <Task other task>]
+        >>> get_objects_for_group(group, ['tasker.change_task'], accept_global_perms=False)
+        [<Task some task>]
+
+    """
+    if isinstance(perms, basestring):
+        perms = [perms]
+    ctype = None
+    app_label = None
+    codenames = set()
+
+    # Compute codenames set and ctype if possible
+    for perm in perms:
+        if '.' in perm:
+            new_app_label, codename = perm.split('.', 1)
+            if app_label is not None and app_label != new_app_label:
+                raise MixedContentTypeError("Given perms must have same app "
+                                            "label (%s != %s)" % (app_label, new_app_label))
+            else:
+                app_label = new_app_label
+        else:
+            codename = perm
+        codenames.add(codename)
+        if app_label is not None:
+            new_ctype = ContentType.objects.get(app_label=app_label,
+                                                permission__codename=codename)
+            if ctype is not None and ctype != new_ctype:
+                raise MixedContentTypeError("ContentType was once computed "
+                                            "to be %s and another one %s" % (ctype, new_ctype))
+            else:
+                ctype = new_ctype
+
+    # Compute queryset and ctype if still missing
+    if ctype is None and klass is None:
+        raise WrongAppError("Cannot determine content type")
+    elif ctype is None and klass is not None:
+        queryset = _get_queryset(klass)
+        ctype = ContentType.objects.get_for_model(queryset.model)
+    elif ctype is not None and klass is None:
+        queryset = _get_queryset(ctype.model_class())
+    else:
+        queryset = _get_queryset(klass)
+        if ctype.model_class() != queryset.model:
+            raise MixedContentTypeError("Content type for given perms and "
+                                        "klass differs")
+
+    # At this point, we should have both ctype and queryset and they should
+    # match which means: ctype.model_class() == queryset.model
+    # we should also have ``codenames`` list
+
+    global_perms = set()
+    if accept_global_perms:
+        global_perm_set = groups.values_list('permissions__codename', flat=True)
+        for code in codenames:
+            if code in global_perm_set:
+                global_perms.add(code)
+        for code in global_perms:
+            codenames.remove(code)
+        if len(global_perms) > 0 and (len(codenames) == 0 or any_perm):
+            return queryset
+
+    # Now we should extract list of pk values for which we would filter queryset
+    group_model = get_group_obj_perms_model(queryset.model)
+    groups_obj_perms_queryset = (group_model.objects
+                                 .filter(group__in=groups)
+                                 .filter(permission__content_type=ctype)
+                                 .filter(permission__codename__in=codenames))
+    if group_model.objects.is_generic():
+        fields = ['object_pk', 'permission__codename']
+    else:
+        fields = ['content_object__pk', 'permission__codename']
+    groups_obj_perms = groups_obj_perms_queryset.values_list(*fields)
+    data = list(groups_obj_perms)
+
+    keyfunc = lambda t: t[0] # sorting/grouping by pk (first in result tuple)
+    data = sorted(data, key=keyfunc)
+    pk_list = []
+    for pk, group in groupby(data, keyfunc):
+        obj_codenames = set((e[1] for e in group))
+        if any_perm or codenames.issubset(obj_codenames):
+            pk_list.append(pk)
+    objects = queryset.filter(pk__in=pk_list)
+    return objects
